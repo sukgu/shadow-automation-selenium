e7c61c90ba589c953b28098eb33bdff0154ab03a
dbryla <d4vos@icloud.com>
2022-04-20 12:44:21 +0200

    reverted BaseBy

--- src/main/java/io/github/sukgu/support/BaseBy.java
+++ src/main/java/io/github/sukgu/support/BaseBy.java
@@ -1,16 +1,17 @@
 package io.github.sukgu.support;
 
-import io.github.sukgu.Shadow;
-import io.github.sukgu.support.FindElementBy.FindByBuilder.SelectorType;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
 
-import java.util.List;
+import io.github.sukgu.Shadow;
+import io.github.sukgu.support.FindElementBy.FindByBuilder.SelectorType;
 
-public abstract class BaseBy extends By {
+abstract class BaseBy extends By {
 
     protected final String selector;
     protected final String selectorType;
@@ -22,58 +23,54 @@ public abstract class BaseBy extends By {
 
     private final WebDriver getWebDriver(SearchContext context) {
         WebDriver webDriver;
-
+        
         if (context instanceof RemoteWebElement) {
-            webDriver = ((RemoteWebElement) context).getWrappedDriver();
+        	webDriver = ((RemoteWebElement) context).getWrappedDriver();
         } else {
-            webDriver = (WebDriver) context;
+        	webDriver = (WebDriver) context;
         }
-
+        
         return webDriver;
     }
 
-    @Override
+	@Override
     public WebElement findElement(SearchContext context) {
-        WebDriver webDriver = getWebDriver(context);
-
+    	WebDriver webDriver = getWebDriver(context);
+    	
         if (context instanceof WebDriver) {
             context = null;
         }
-
+        
         Shadow shadow = new Shadow(webDriver);
-
-        if (selectorType.equals(SelectorType.CSS_SELECTOR.toString())) {
-            return shadow.findElement(selector);
+        
+        if(selectorType.equals(SelectorType.CSS_SELECTOR.toString())) {
+        	return shadow.findElement(selector);
         }
-
-        if (selectorType.equals(SelectorType.XPATH.toString())) {
-            return shadow.findElementByXPath(selector);
+        
+        if(selectorType.equals(SelectorType.XPATH.toString())) {
+        	return shadow.findElementByXPath(selector);
         }
-
+        
         return null;
     }
 
-    @Override
+	@Override
     public List<WebElement> findElements(SearchContext context) {
-        WebDriver webDriver = getWebDriver(context);
-
+		WebDriver webDriver = getWebDriver(context);
+		
         if (context instanceof WebDriver) {
             context = null;
         }
-
+        
         Shadow shadow = new Shadow(webDriver);
-        if (selectorType.equals(SelectorType.CSS_SELECTOR.toString())) {
-            return shadow.findElements(selector);
+        if(selectorType.equals(SelectorType.CSS_SELECTOR.toString())) {
+        	return shadow.findElements(selector);
         }
-
-        if (selectorType.equals(SelectorType.XPATH.toString())) {
-            return shadow.findElementsByXPath(selector);
+        
+        if(selectorType.equals(SelectorType.XPATH.toString())) {
+        	return shadow.findElementsByXPath(selector);
         }
-
+        
         return null;
-    }
-
-    public String getSelector() {
-        return selector;
     }
 }
